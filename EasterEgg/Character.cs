766fd5ca1c720219dc5d58358e1113541f9d8393
JanEspana <jan.espana.7e7@itb.cat>
2024-03-20 12:04:35 +0100

    aaa

--- EasterEgg/Character.cs
+++ EasterEgg/Character.cs
@@ -8,6 +8,30 @@
         public int Atk { get; set; }
         public int Def { get; set; }
         public int Spd { get; set; }
-        public int Exp { get; set; }
+
+        public Character(string? name, int health, int atk, int def, int spd)
+        {
+            Name = name;
+            Level = 1;
+            Health = health;
+            Atk = atk;
+            Def = def;
+            Spd = spd;
+        }
+        public static void CreateCharacter()
+        {
+            Console.WriteLine("Enter your character's name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your character's health:");
+            int health = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your character's attack:");
+            int atk = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your character's defense:");
+            int def = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your character's speed:");
+            int spd = Convert.ToInt32(Console.ReadLine());
+
+            Character player = new Character(name, health, atk, def, spd);
+        }
     }
 }
