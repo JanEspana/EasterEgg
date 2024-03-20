a6133ec713665d7a2649eb26459ddee8d54f4fa3
JanEspana <jan.espana.7e7@itb.cat>
2024-03-20 11:54:06 +0100

    Add project files.

---
+++ EasterEgg/Character.cs
@@ -0,0 +1,13 @@
+﻿namespace EasterEgg
+{
+    public class Character
+    {
+        public string? Name { get; set; }
+        public int Level { get; set; }
+        public int Health { get; set; }
+        public int Atk { get; set; }
+        public int Def { get; set; }
+        public int Spd { get; set; }
+        public int Exp { get; set; }
+    }
+}
---
+++ EasterEgg/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EasterEgg
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
