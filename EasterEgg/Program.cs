cc67bda1a2bec56413a5e91b28fa138ec4d41450
Raúl Palomo Sánchez <146939183+RaulPalomo@users.noreply.github.com>
2024-03-20 12:25:24 +0100

    Update Program.cs

--- EasterEgg/Program.cs
+++ EasterEgg/Program.cs
@@ -1,10 +1,31 @@
-﻿using System;
+using System;
+using System.Xml;
+
 namespace EasterEgg
 {
     public class Program
     {
         public static void Main()
         {
+
+            const string MsgWelcome = "Hello hero! Do you want to create a character? \n1.YES\n2.NO";
+            const string again = "Do you want to create another?\n1.YES\n2.NO";
+            const string byebye = "Thanks for using. Here are your characters";
+
+            int selection = 1;
+            
+            Console.WriteLine(MsgWelcome);
+            selection = Convert.ToInt32(Console.ReadLine());
+            while (selection == 1) {               
+                Character.CreateCharacter();
+                Console.WriteLine(again);
+                selection = Convert.ToInt32(Console.ReadLine());             
+            }
+            if (selection < 1 || selection > 2) Console.WriteLine("You are have stupid");
+            if (selection == 2) Console.WriteLine(byebye);
+
+            
+
 
         }
     }
